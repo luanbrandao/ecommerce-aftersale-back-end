3c3c79e632e88f79573360690f03e0c46d09d423
luanbrandao <luanbrandao4@gmail.com>
2020-08-27 14:24:13 -0300

    refactor: ensure FakeHashProvider calls generateHash returns number random

--- src/modules/users/providers/HashProvider/fakes/FakeHashProvider.ts
+++ src/modules/users/providers/HashProvider/fakes/FakeHashProvider.ts
@@ -1,8 +1,10 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import faker from 'faker';
 import IHashProvider from '../models/IHashProvider';
 
 class FakeHashProvider implements IHashProvider {
-  public async generateHash(playload: string): Promise<string> {
-    return playload;
+  public async generateHash(_playload: string): Promise<string> {
+    return faker.random.number().toString();
   }
 
   public async compareHash(playload: string, hashed: string): Promise<boolean> {
