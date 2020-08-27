ae633cd5b64b85c43e211261e65f17e2ec904047
luanbrandao <luanbrandao4@gmail.com>
2020-08-27 20:20:06 -0300

    feat: create entity Favorite

---
+++ src/modules/favorites/infra/typeorm/entities/Favorite.ts
@@ -0,0 +1,42 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+  CreateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+
+// KISS - Keep It Simple & Stupid
+import User from '@modules/users/infra/typeorm/entities/User';
+
+@Entity('favorites')
+class Favorite {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  product_id: string;
+
+  @Column()
+  title: string;
+
+  @Column()
+  price: number;
+
+  @Column()
+  image_url: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default Favorite;
