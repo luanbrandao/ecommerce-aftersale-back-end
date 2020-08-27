d7704ec9b43f325abbf7dfb0a08d09a941cf3590
luanbrandao <luanbrandao4@gmail.com>
2020-08-27 17:04:56 -0300

    feat: add validations router session user

--- src/modules/users/infra/http/routes/sessions.routes.ts
+++ src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,9 +1,19 @@
 import { Router, Response, Request } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 const sessionsRouter = Router();
 
-sessionsRouter.post('/', (_request: Request, response: Response) => {
-  return response.json({ message: 'session' });
-});
+sessionsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  (_request: Request, response: Response) => {
+    return response.json({ message: 'session' });
+  },
+);
 
 export default sessionsRouter;
