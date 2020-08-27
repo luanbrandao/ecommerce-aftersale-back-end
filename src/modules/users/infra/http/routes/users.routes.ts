40a4cf20cfe535e86430c9a4e74d7f07e3b33d81
luanbrandao <luanbrandao4@gmail.com>
2020-08-26 22:30:48 -0300

    feat: ensure CreateUserService calls generateHash with correct password

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import CreateUserService from '../../../services/CreateUserService';
 import UsersRepository from '../../typeorm/repositories/UsersRepository';
+import BCryptHashProvider from '../../../providers/HashProvider/implementations/BCryptHashProvider';
 
 const userRouter = Router();
 
@@ -9,7 +10,11 @@ userRouter.post('/', async (request, response) => {
     const { name, email, password } = request.body;
 
     const usersRepository = new UsersRepository();
-    const createUser = new CreateUserService(usersRepository);
+    const bCryptHashProvider = new BCryptHashProvider();
+    const createUser = new CreateUserService(
+      usersRepository,
+      bCryptHashProvider,
+    );
     const user = await createUser.execute({
       name,
       email,
--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,5 +1,6 @@
 import User from '../infra/typeorm/entities/User';
 import IUsersRepository from '../repositories/IUsersRepository';
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
 
 interface IRequest {
   name: string;
@@ -8,7 +9,10 @@ interface IRequest {
 }
 
 export default class CreateUserService {
-  constructor(private usersRepository: IUsersRepository) {}
+  constructor(
+    private usersRepository: IUsersRepository,
+    private iHashProvider: IHashProvider,
+  ) {}
 
   async execute({ name, email, password }: IRequest): Promise<User> {
     const checkUserExists = await this.usersRepository.findByEmail(email);
@@ -17,10 +21,12 @@ export default class CreateUserService {
       throw Error('Email address alredy used.');
     }
 
+    const hashedPassword = await this.iHashProvider.generateHash(password);
+
     const user = this.usersRepository.create({
       name,
       email,
-      password,
+      password: hashedPassword,
     });
 
     return user;
