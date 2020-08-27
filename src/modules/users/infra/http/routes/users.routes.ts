94efeefbcd1319ac54b1a4d914422cac775eafc3
luanbrandao <luanbrandao4@gmail.com>
2020-08-27 10:52:41 -0300

    feat: add validations router create user

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,10 +1,24 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 import UsersController from '../controllers/UsersController';
 
 const userRouter = Router();
 
 const usersController = new UsersController();
 
-userRouter.post('/', usersController.create);
+// userRouter.post('/', usersController.create);
+
+userRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+      password_confirmation: Joi.string().valid(Joi.ref('password')),
+    },
+  }),
+  usersController.create,
+);
 
 export default userRouter;
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,7 +1,9 @@
 import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
+// add 'express-async-errors' after express
 import 'express-async-errors';
 import '../typeorm';
+import { errors } from 'celebrate';
 import AppError from '../../errors/AppError';
 import routes from './routes';
 
@@ -14,6 +16,9 @@ app.post('/test', (requesnt, response) => {
   return response.json({ user: 'user' });
 });
 
+app.use(errors());
+
+// add erros after routes
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
     return response.status(err.statusCode).json({
@@ -22,7 +27,6 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
     });
   }
 
-  console.log(err);
   return response.status(500).json({
     status: 'error',
     message: err.message,
